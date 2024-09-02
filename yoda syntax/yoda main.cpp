1cd977555f8be80ceda86bc5dd14e3b579e20714
P <petrubraha@gmail.com>
2024-09-02 22:21:19 +0300

    initial idea

---
+++ yoda syntax/yoda main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	/*
+	cin >> text;
+	brutally modify text / modify and test if it is the same output
+	*/
+}
+
+/*
+@input: large text
+@output: corrected text with correct specifications
+
+@specifications:
+- all relational operator's members in correct order
+(primitives first)
+- correct accidental assignements in if and while statements
+- will try to compile and expect the same output 
+*/
